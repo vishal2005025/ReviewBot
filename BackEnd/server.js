45f428b1eca647da2f800298f2ebf57501b10af6
vishal2005025 <vishalmalyan2005@gmail.com>
2025-05-12 18:08:16 +0530

    added url backend

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,5 +4,5 @@ const app = require('./src/app')
 
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on https://reviewbot-2uk8.onrender.com/')
 })
